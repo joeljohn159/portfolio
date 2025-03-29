490139327e593f27206eabe40034dc720349202d
joeljohn159 <86117657+joeljohn159@users.noreply.github.com>
2025-03-29 13:10:32 -0400

    Update Home.jsx

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,16 +1,24 @@
+import {useEffect, useState} from 'react';
 import './Home.css';
 import Social from './Social';
 import Data from './Data';
 import ScrollDown from './ScrollDown';
 
+
 export default function Home(){
+    const [isVisible, setIsVisible] = useState(false);
+
+    useEffect(() => {
+        setIsVisible(true);
+    }, []);
+
     return (
         <section className="home section" id="home">
             <div className="home__container container grid">
                 <div className="home__content grid">
                     <Social />
 
-                    <div className="home__img"></div>
+                    <div className={`home__img ${isVisible ? "visible" : "hidden"}`}></div>
 
                     <Data />
                 </div>
